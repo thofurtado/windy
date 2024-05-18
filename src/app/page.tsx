39ea3afedf40510f91099585be6ac7abda112e0a
Thomás Furtado <thofurtado@gmail.com>
2024-05-18 12:37:02 -0300

    before nextui instalation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,19 @@
 import { invoke } from '@tauri-apps/api/tauri'
 import { arch, platform } from '@tauri-apps/api/os';
 import { Command } from '@tauri-apps/api/shell'
+import { useState } from 'react';
 
 export default function Home() {
-
+  const [platformLabel, setPlatformLabel] = useState('Indeterminada')
   async function teste() {
     invoke('os_info').then((message) => console.log(message))
+    setPlatformLabel(await platform())
+
   }
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <button onClick={teste}>send</button>
+      <label>Plataforma: {platformLabel}</label>
     </main>
   );
 }
