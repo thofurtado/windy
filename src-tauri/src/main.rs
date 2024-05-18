b1e1d3bb4ca41157b9e9a0f7c0bf9aa0a5e3ddc4
Thomás Furtado <32690619+thofurtado@users.noreply.github.com>
2024-05-18 08:04:03 -0300

    minimum comunication

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,16 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+
+
+#[tauri::command]
+fn os_info()  -> String {
+  "Hello from Rust!".into()
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![os_info])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
